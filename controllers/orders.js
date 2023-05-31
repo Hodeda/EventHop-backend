36a42f5b315c5b7108f316b5cf0bdfedd888ebb6
Hodeda <orhodeda@gmail.com>
2023-05-31 20:16:31 +0300

    added prediction server

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,58 +1,74 @@
 const asyncHandler = require("express-async-handler");
 const { Order } = require("../models/orders");
 const { sendEmail } = require("./utils");
+const axios = require("axios");
 
 exports.getAll = asyncHandler(async (req, res) => {
-    const allOrders = await Order.find();
-    res.status(200).send(allOrders);
+  const allOrders = await Order.find();
+  res.status(200).send(allOrders);
 });
 
 exports.getById = asyncHandler(async (req, res) => {
-    const id = req.params.id;
-    const order = await Order.findById(id);
-    res.status(200).send(order);
+  const id = req.params.id;
+  const order = await Order.findById(id);
+  res.status(200).send(order);
 });
 
 exports.calculatePrice = asyncHandler(async (req, res) => {
-    /*
-    // add when there is a real algorithm
-    // take input from body (this is a POST method handler)
-    const { name, destination } = req.body;
+  console.log(req.body);
+  const {
+    pickup_longitude,
+    pickup_latitude,
+    dropoff_longitude,
+    dropoff_latitude,
+    passenger_count,
+    Pickup_Year,
+    Pickup_Month,
+    Pickup_Day,
+    Pickup_Hour,
+    Pickup_Minute,
+    Pickup_DayOfWeek,
+    Euclidean_Distance,
+    DayHour,
+  } = req.body;
 
-    // validate input
-    if (!(name && destination)) {
-        return res.status(400).send("All input is required");
-    }
-    */
+  const baseUrl = "http://3.70.175.138:8080/predict_flare";
 
-    function randomNumber(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min)
-    }
+  const url = `${baseUrl}?pickup_longitude=${pickup_longitude}&pickup_latitude=${pickup_latitude}&dropoff_longitude=${dropoff_longitude}&dropoff_latitude=${dropoff_latitude}&passenger_count=${passenger_count}&Pickup_Year=${Pickup_Year}&Pickup_Month=${Pickup_Month}&Pickup_Day=${Pickup_Day}&Pickup_Hour=${Pickup_Hour}&Pickup_Minute=${Pickup_Minute}&Pickup_DayOfWeek=${Pickup_DayOfWeek}&Euclidean_Distance=${Euclidean_Distance}&DayHour=${DayHour}`;
 
-    const result = randomNumber(50, 150);
-    res.status(200).send({result: result});
+  try {
+    const response = await axios.get(url);
+    console.log(response.data); // Log the data from the response
+    res.status(200).json(response.data); // Respond with the data from the axios request
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while calculating the price." });
+  }
 });
 
 exports.post = async (req, res) => {
-    const { firstName, lastName, email, phone, passengers, arrivalTime, price } = req.body;
-    if (!(firstName && lastName && email && phone && passengers && arrivalTime)) { //todo: later add price to also be required
-        return res.status(400).send("All input is required");
-    }
+  const { firstName, lastName, email, phone, passengers, arrivalTime, price } =
+    req.body;
+  if (!(firstName && lastName && email && phone && passengers && arrivalTime)) {
+    //todo: later add price to also be required
+    return res.status(400).send("All input is required");
+  }
 
-    const newOrder = new Order(req.body);
-    try {
-        const savedOrder = await newOrder.save();
-        // sendEmail(email, savedOrder);
-    
-        res.status(200).send(savedOrder);
-    }
-    catch (e) {
-        res.status(500).send(e);
-    }
+  const newOrder = new Order(req.body);
+  try {
+    const savedOrder = await newOrder.save();
+    // sendEmail(email, savedOrder);
+
+    res.status(200).send(savedOrder);
+  } catch (e) {
+    res.status(500).send(e);
+  }
 };
 
 exports.deleteById = asyncHandler(async (req, res) => {
-    const id = req.params.id;
-    const deleteResult = await Order.deleteOne({ _id: id });
-    res.status(200).send(deleteResult);
+  const id = req.params.id;
+  const deleteResult = await Order.deleteOne({ _id: id });
+  res.status(200).send(deleteResult);
 });
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require('cors');
+const cors = require("cors");
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 const URI = process.env.URI;
@@ -8,27 +8,29 @@ const ordersRoute = require("./routes/orders");
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: ['http://localhost:3000'] //don't forget to add cors for the deployed version's url!
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:3001"], //don't forget to add cors for the deployed version's url!
+  })
+);
 
-app.use('/order', ordersRoute);
+app.use("/order", ordersRoute);
 
-
-app.use("*", (req, res) => res.status(404).json({ message: "Page not found." }));
+app.use("*", (req, res) =>
+  res.status(404).json({ message: "Page not found." })
+);
 
 main();
 
-async function main () {
+async function main() {
   try {
-      await mongoose.connect(URI);
-      console.log("connected to db");
-      const server = app.listen(PORT, () => {
-          console.log(`Listening on port ${PORT}...`)
-      })
-  }
-  catch (err) {
-      console.log(err);
-      process.exit(1);
+    await mongoose.connect(URI);
+    console.log("connected to db");
+    const server = app.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}...`);
+    });
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
   }
 }
